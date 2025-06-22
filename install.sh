d3b4af6618c3f0f3ab8d3654884c6b283141c451
尘漾 <chenyang.yl@alibaba-inc.com>
2025-06-22 18:38:01 +0800

    feat: support zsh

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
 mkdir -p $HOME/.uvc
-wget -O $HOME/.uvc/uvc.sh 'uvc.sh'
+wget -O $HOME/.uvc/uvc.sh 'https://github.com/LiangYang666/uvc/releases/download/latest/uvc.sh'
 
 enable_uvc='source "$HOME/.uvc/uvc.sh"'
 
-if ! grep -qxF "$enable_uvc" ~/.bashrc; then
-    echo "$enable_uvc" >> ~/.bashrc
+if [ -n "$ZSH_VERSION" ]; then
+    config_file="$HOME/.zshrc"
+elif [ -n "$BASH_VERSION" ]; then
+    config_file="$HOME/.bashrc"
+else
+    config_file="$HOME/.bashrc"
 fi
-source ~/.bashrc
+
+# 检查并添加配置
+if ! grep -qxF "$enable_uvc" "$config_file"; then
+    echo "$enable_uvc" >> "$config_file"
+fi
+
+# 重新加载配置
+source "$config_file"
